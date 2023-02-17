92a5e62403ca0462f7db1c3fec5d442564744e81
Macroshah <68460973+shahbasharat@users.noreply.github.com>
2023-02-17 06:51:35 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,17 +190,7 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/63eed1f2474251287913cef6/1gpee87l7';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-// End of Tawk.to Live Chat
+
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
